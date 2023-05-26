df949d179cb008fa057aa475d66db2e492800e80
Frederik Duchi <frederik.duchi@howest.be>
2023-05-26 06:49:02 +0000

    added layout search form

--- pages/Search/jsobjects/DataParser/DataParser.js
+++ pages/Search/jsobjects/DataParser/DataParser.js
@@ -1,17 +1,13 @@
 export default {
 	myVar1: [],
 	myVar2: {},
-	getSectors: () => {	
-		return GetSectors.data.results.map(item => {
+	getCategories: () => {	
+		return GetCategories.data.results.map(item => {
 			const obj = Object.assign({}, item);
-      obj['label'] = item.Name;
-			obj['value'] = item.Name;
-      return obj;		
+			obj['label'] = item.Category_English;
+			obj['value'] = item.id;
+			return obj;		
 		})
-	},
-	myFun1 () {
-		//	write code here
-		//	this.myVar1 = [1,2,3]
 	},
 	async myFun2 () {
 		//	use async-await or promises
