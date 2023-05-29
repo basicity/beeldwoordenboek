343aad3a9fe8a9b2dd66eb862db0a716b38fc11c
Frederik Duchi <frederik.duchi@howest.be>
2023-05-29 05:17:01 +0000

    implemented favourites

---
+++ pages/SearchDetails/jsobjects/Favourites/Favourites.js
@@ -0,0 +1,17 @@
+export default {
+	myVar1: [],
+	myVar2: {},
+	async ChangeFavouritesState () {
+		if(switchFavourite.isSwitchedOn){
+			// add the id to the array
+			appsmith.store.user.Favourites.push(parseInt(idField.text))
+		}else{
+			console.log('switch off');
+			const index = appsmith.store.user.Favourites.indexOf(parseInt(idField.text));
+			if(index > -1){
+				appsmith.store.user.Favourites.splice(index,1)
+			}
+		}
+		const result = await UpdateFavourites.run();
+	}
+}
