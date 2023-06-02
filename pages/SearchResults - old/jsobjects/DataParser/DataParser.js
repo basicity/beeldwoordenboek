f9007c95f135960892bb4782aa64f1318d28909c
Frederik Duchi <frederik.duchi@howest.be>
2023-06-02 10:05:06 +0000

    added menu on top

--- pages/SearchResults - old/jsobjects/DataParser/DataParser.js
+++
@@ -1,8 +0,0 @@
-export default {
-	myVar1: [],
-	myVar2: {},
-	filterTermsByCategory () {
-		//	use async-await or promises
-		return GetTermsBySearch.data.results.filter(t => appsmith.URL.queryParams.categories.includes(t.Category[0].id));
-	}
-}
