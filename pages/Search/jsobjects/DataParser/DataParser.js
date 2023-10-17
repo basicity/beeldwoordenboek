e9ac5008156263108a3fa9253a6c91177aa6614a
Frederik Duchi <frederik.duchi@howest.be>
2023-10-17 12:10:22 +0000

    changed language settings

--- pages/Search/jsobjects/DataParser/DataParser.js
+++ pages/Search/jsobjects/DataParser/DataParser.js
@@ -2,7 +2,7 @@ export default {
 	getCategories: () => {	
 		return GetCategories.data.results.map(item => {
 			const obj = Object.assign({}, item);
-			obj['label'] = item.Category_English;
+			obj['label'] = item[`Category_${appsmith.store.user ? appsmith.store.user.InterfaceLanguage.value : 'English'}`];
 			obj['value'] = item.id;
 			return obj;		
 		})
