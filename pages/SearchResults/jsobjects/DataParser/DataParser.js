d5f70731c30f1c1d8ee2fe4aa7811f5947a748ad
Frederik Duchi <frederik.duchi@howest.be>
2023-10-18 12:41:38 +0000

    fixed category filter

--- pages/SearchResults/jsobjects/DataParser/DataParser.js
+++ pages/SearchResults/jsobjects/DataParser/DataParser.js
@@ -1,8 +1,25 @@
 export default {
-	myVar1: [],
-	myVar2: {},
-	filterTermsByCategory () {
+	page: 1,
+	filteredTermsByCategory: [],
+	resetPage(){ 
+		this.page = 1
+	},
+	async filterTermsByCategory () {
 		//	use async-await or promises
-		return GetTermsBySearch.data.results.filter(t => appsmith.URL.queryParams.categories.includes(t.Category[0].id));
+		const terms = [];
+		this.page = 1;
+		const data = await GetTermsBySearch.run();
+		data.results.forEach(t => terms.push(t));
+
+
+		const pages = Math.ceil(data.count / 100);
+		
+		for(this.page = 2; this.page <= pages; this.page++){
+			const data = await GetTermsBySearch.run();
+			data.results.forEach(t => terms.push(t));
+		}
+		
+		this.filteredTermsByCategory = terms.filter(t => appsmith.URL.queryParams.categories.includes(t.Category[0].id));
+		console.log(this.filteredTermsByCategory)
 	}
 }
